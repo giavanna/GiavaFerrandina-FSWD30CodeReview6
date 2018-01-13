933f6807d76a81387e060fb5a4e39d56ea67712c
Giavanna <giava.ferrandina@gmail.com>
2018-01-13 16:44:12 +0100

    code review 6

---
+++ GiavaFerrandinaFSWD30CodeReview6.js
@@ -0,0 +1,124 @@
+var star = "<i class=\"glyphicon glyphicon-star\"></i>"
+var starEmpty = "<i class=\"glyphicon glyphicon-star-empty\"></i>"
+var Books = [
+	{
+	Title: "The da Vinci Code",
+	Author : "Dan Brown",
+	Genre : "Novel",
+	Publisher: "Doubleday (US) Transworld & Bantam Books (UK)",
+	Photo: "img\\ilcodicedavinci.jpg",
+	Rating : star+star+star+star+starEmpty
+	},
+
+	{
+	Title: "L'alchimista",
+	Author : "Paolo Coelho",
+	Genre : "Roman",
+	Publisher: "Harper Collins",
+	Photo: "img\\lalchimista.jpg",
+	Rating : star+starEmpty+star+star+starEmpty
+	},
+
+	{
+	Title: "Harry Potter e la pietra filosofale. 1.",
+	Author : "Joanne K. Rowling ",
+	Genre : "Fantasy",
+	Publisher: "Scholastic Corporation",
+	Photo: "img\\potter.jpg",
+	Rating : star+star+starEmpty+star+starEmpty
+	},
+
+	{
+	Title: "Braveheart",
+	Author : "Randall Wallace ",
+	Genre : "Novel",
+	Publisher: "Randall Wallace",
+	Photo: "img\\Braveheartht.jpg",
+	Rating : starEmpty+star+star+star+starEmpty
+	},
+	
+];
+
+
+
+var Dvds = [
+	{
+	Title: "Herr der Ringe",
+	Author : "J. R. R. Tolkien",
+	Genre : "High Fantasy",
+	Publisher: "J. R. R. Tolkien",
+	Photo: "img\\Herr-der-ringe1.jpg",
+	Rating : star+star+starEmpty+star+starEmpty
+	},
+
+	{
+	Title: "Der Hobbit",
+	Author : "J. R. R. Tolkien",
+	Genre : "Fantasyroman",
+	Publisher: "Allen & Unwin",
+	Photo: "img\\hobbit.jpg",
+	Rating : starEmpty+star+star+star+starEmpty
+	},
+
+	{
+	Title: "Die Chroniken von Narnia",
+	Author : "C. S. Lewis",
+	Genre : "Fantasyroman",
+	Publisher: "Harper Collins",
+	Photo: "img\\narnia.jpg",
+	Rating : star+star+star+starEmpty+starEmpty
+	},
+
+	{
+	Title: "The king's Speech",
+	Author : "Tom Hooper ",
+	Genre : "biographical , dramatic , historical",
+	Publisher: "Sterling Publishing",
+	Photo: "img\\kingsSpeech.jpg",
+	Rating : star+star+starEmpty+star+starEmpty
+	},
+];
+
+
+var Cds = [
+	{
+	Title: "Feels like Home",
+	Author : "Nora Jones",
+	Genre : "Pop Vocal",
+	Publisher: "Blue Note Records",
+	Photo: "img\\nora-jones.jpg",
+	Rating : star+starEmpty+star+star+starEmpty
+	},
+
+	{
+	Title: "L'anima Vola",
+	Author : "Elisa",
+	Genre : "Pop rock",
+	Publisher:"Sugar Music", 
+	Photo: "img\\Elisa.jpg",
+	Rating : star+star+star+star+starEmpty
+	},
+
+	{
+	Title: "Black cat",
+	Author : "Zucchero Fornaciari",
+	Genre : "Blues rock",
+	Publisher: "Universal Music Oy",
+	Photo: "img\\zucchero.jpg",
+	Rating : star+star+star+star+starEmpty
+	},
+
+	{
+	Title: "New Beginning",
+	Author : "Tracy Chapman ",
+	Genre : "Pop",
+	Publisher: "Elektra Records",
+	Photo: "img\\Tracy Chapman.jpg",
+	Rating : star+star+star+star+starEmpty
+	},
+];
+
+
+
+
+	
